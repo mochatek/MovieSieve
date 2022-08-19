31b0234cff3f2edb02632016dcd29b6d271782cd
mochatek <akashsabu@ymail.com>
2022-08-19 23:09:36 +0530

    fix(UI): extract and send data on addMovie

--- UI/src/lib/components/AddMovie.svelte
+++ UI/src/lib/components/AddMovie.svelte
@@ -2,8 +2,55 @@
   import { movies, selectedMovie } from "../../store";
 
   const addMovie = () => {
-    movies.addMovie($selectedMovie);
+    movies.addMovie(
+      $selectedMovie,
+      {
+        Certificate,
+        Genre,
+        Runtime,
+        Release,
+        Country,
+        Language,
+        imdbRating,
+        Director,
+        Writer,
+        Actors,
+        Awards,
+        Plot,
+      },
+      Poster
+    );
   };
+
+  let Certificate = "";
+  let Genre = "";
+  let Runtime = "";
+  let Release = "";
+  let Country = "";
+  let Language = "";
+  let imdbRating = "";
+  let Director = "";
+  let Writer = "";
+  let Actors = "";
+  let Awards = "";
+  let Poster = "";
+  let Plot = "";
+
+  $: disabled = [
+    Certificate,
+    Genre,
+    Runtime,
+    Release,
+    Country,
+    Language,
+    imdbRating,
+    Director,
+    Writer,
+    Actors,
+    Awards,
+    Poster,
+    Plot,
+  ].some((input) => input.trim() == "");
 </script>
 
 <header class="info">
@@ -13,30 +60,32 @@
 </header>
 <form id="add-movie">
   <div class="row">
-    <input type="text" placeholder="Name" readonly value={$selectedMovie} />
-    <input type="text" placeholder="Certificate" />
-    <input type="text" placeholder="Genre" />
-    <input type="text" placeholder="Runtime" />
+    <input type="text" readonly value={$selectedMovie} />
+    <input type="text" placeholder="Certificate" bind:value={Certificate} />
+    <input type="text" placeholder="Genre" bind:value={Genre} />
+    <input type="text" placeholder="Runtime" bind:value={Runtime} />
   </div>
   <div class="row">
-    <input type="text" placeholder="Release" />
-    <input type="text" placeholder="Country" />
-    <input type="text" placeholder="Language" />
-    <input type="text" placeholder="IMDb Rating" />
+    <input type="text" placeholder="Release" bind:value={Release} />
+    <input type="text" placeholder="Country" bind:value={Country} />
+    <input type="text" placeholder="Language" bind:value={Language} />
+    <input type="text" placeholder="IMDb Rating" bind:value={imdbRating} />
   </div>
   <div class="row">
-    <input type="text" placeholder="Director" />
-    <input type="text" placeholder="Writer" />
-    <input type="text" placeholder="Actors" />
-    <input type="text" placeholder="Awards" />
+    <input type="text" placeholder="Director" bind:value={Director} />
+    <input type="text" placeholder="Writer" bind:value={Writer} />
+    <input type="text" placeholder="Actors" bind:value={Actors} />
+    <input type="text" placeholder="Awards" bind:value={Awards} />
   </div>
   <div class="row extend">
-    <input type="text" placeholder="Poster URL" />
+    <input type="text" placeholder="Poster URL" bind:value={Poster} />
   </div>
   <div class="row extend">
-    <textarea placeholder="Plot Summary" />
+    <textarea placeholder="Plot Summary" bind:value={Plot} />
   </div>
   <div class="row">
-    <button type="submit" on:click|preventDefault={addMovie}>Save</button>
+    <button type="submit" class:disabled on:click|preventDefault={addMovie}
+      >Save</button
+    >
   </div>
 </form>
